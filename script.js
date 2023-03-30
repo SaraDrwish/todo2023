3da310aabd51c827bdb4ce7bba9e8e45cadcf0a7
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-03-31 00:27:31 +0300

    js3

--- script.js
+++ script.js
@@ -1,77 +1,32 @@
-// var decleration
-
-///////////////////////////////////////////////////////////////////////
-
-//functions and events
-
-// document.addEventListener("DOMContentLaoded", getTodos);
-
-// btn.addEventListener("click", add);
-
-// listUl.addEventListener("click", del);
-
-// selectTodo.addEventListener("change", filtr);
-
-
-///////////////////////////////////////////////////////////////////////
-
-// declare functions
-
-
-//1
-
-// function add(evnt) {
-//    evnt.preventDefault();
-//     const listli = document.createElement("li");
-//     listli.innerText = inp.value;
-//     listli.classList.add("botom-listli")
-
-//     saveLocaltodos(inp.value);
-
-
-// }
-
-
-
-window.addEventListener('load', () => {
-
     const form = document.querySelector("form");
     const inp = document.querySelector("form .input-form");
     const btn = document.querySelector(".btn");
     const listUl = document.querySelector(".botom-list-todo");
-    const selectTodo = document.querySelector(".todos");
+    const todosfltr = document.querySelector(".todos");
+    
+window.addEventListener('load', () => {
 
-    // const listli = document.querySelector(".botom-listli");  
-    // const liP = document.querySelector(".liP")
-
+   
 
     document.addEventListener("submit", (e) => {
         e.preventDefault();
 
         const task = inp.value;
 
-        if ( !(task) ) {
+        if (!(task)) {
             alert("it is empty");
             return;
         }
-        // else {
-        //     console.log("success");
-        // }
 
-        // listUl.style.display = "block";
-        // liP.innerText = task;
         const listli = document.createElement("li");
         listli.classList.add("botom-listli");
         listUl.style.display = "block";
-
-      
 
         const liP = document.createElement("input");
         liP.classList.add("liP");
         liP.type = "text";
         liP.value = task;
-        liP.setAttribute("readonly" , "readonly")
-        // liP.innerText = task;
+        liP.setAttribute("readonly", "readonly")
         listli.appendChild(liP);
 
         const iconBox = document.createElement("div");
@@ -85,7 +40,7 @@ window.addEventListener('load', () => {
         rightbtn.classList.add("i2");
         rightbtn.innerHTML = ` <i class="fa fa-check"> </i> `;
 
-         const editbtn = document.createElement("i");
+        const editbtn = document.createElement("i");
         editbtn.classList.add("i3");
         editbtn.innerHTML = ` <i class="fa fa-edit"> </i> `;
 
@@ -94,73 +49,130 @@ window.addEventListener('load', () => {
         iconBox.appendChild(rightbtn);
         listli.appendChild(iconBox);
 
-
         listUl.appendChild(listli);
 
         inp.value = "";
 
         delbtn.addEventListener("click", () => {
-            // listli.style.display = "none";
             listUl.removeChild(listli);
         });
 
-        rightbtn.addEventListener("click", () => {
+        function toggleRightBtn(e) {
+            listli.classList.toggle("rightbtnTogl");
+            iconBox.style.background = "unset"; 
+            // listli.style.color = " #8ead85";
+        }
+        rightbtn.addEventListener("click", toggleRightBtn);
 
-            if (rightbtn.innerHTML.toLocaleLowerCase() == ` <i class="fa fa-check"> </i> `) {
-                 listli.style.backgroundColor = "#b8ceb2dc";
-                iconBox.style.background= "transparent";
-                editbtn.style.background = "#b8ceb2dc";
-                rightbtn.style.background = "#b8ceb2dc";
-                delbtn.style.background = "#b8ceb2dc";
+        editbtn.addEventListener("click", () => {
 
-            }else{
-                listli.style.backgroundColor = "#fce283";
-                iconBox.style.background= "fce283";
-                editbtn.style.background = "#ffd000";
-                rightbtn.style.background = "#ffd000";
-                delbtn.style.background = "#ffd000";
-            }
-                
- 
-        });
-
-         editbtn.addEventListener("click", ( ) => {
-
-             if (editbtn.innerHTML.toLocaleLowerCase() == ` <i class="fa fa-edit"> </i> `) {
+            if (editbtn.innerHTML.toLocaleLowerCase() == ` <i class="fa fa-edit"> </i> `) {
                  
                 listli.style.backgroundColor = "#abedf1c6";
-                iconBox.style.background= "transparent";
+                iconBox.style.background = "transparent";
                 editbtn.style.background = "#abedf1c6";
                 rightbtn.style.background = "#abedf1c6";
-                 delbtn.style.background = "#abedf1c6";
+                delbtn.style.background = "#abedf1c6";
                  
-                 liP.focus();
-                 liP.removeAttribute("readonly");
+                liP.focus();
+                liP.removeAttribute("readonly");
                 
-                 editbtn.innerHTML = ` <i class="fa fa-save"> </i> `;
-             } else {
-                 liP.setAttribute("readonly", "readonly");
-                 editbtn.innerHTML = ` <i class="fa fa-edit"> </i> `;
+                editbtn.innerHTML = ` <i class="fa fa-save"> </i> `;
+            } else {
+                liP.setAttribute("readonly", "readonly");
+                editbtn.innerHTML = ` <i class="fa fa-edit"> </i> `;
 
                 listli.style.backgroundColor = "#fce283";
-                iconBox.style.background= "fce283";
+                iconBox.style.background = "fce283";
                 editbtn.style.background = "#ffd000";
                 rightbtn.style.background = "#ffd000";
                 delbtn.style.background = "#ffd000";
                 
-             }
- 
+            }
+
+// end editbtn.addEventListener("click"
+        })
+        
+
+
+
+      
+todosfltr.addEventListener("change", selectTodofilter );
+
+function selectTodofilter(e) {
+    const todos = listUl.childNodes;
+    console.log(todos);
+
+    // const item = e.target;
+    // const todo = item.parentElement;
+
+    todos.forEach(function(t) {
+        switch(e.target.value) {
+            case "all": 
+                listli.style.display = "flex";
+                break;
+            case "dont":
+                if(!t.classList.contains('rightbtnTogl')) {
+                    listli.style.display = "flex";
+                } else {
+                    listli.style.display = "none";
+                }
+                break;
+            case "done": 
+                if(t.classList.contains('rightbtnTogl')) {
+                    listli.style.display = "flex";
+                } else {
+                    listli.style.display = "none";
+                }
+                break;
             
+        }
+    });
+    }  
 
-         })
-        
-        
-       
-        
 
+ //ADDING TO LOCAL STORAGE 
+    // saveLocalTodos(inp.value);
+
+
+//end document.addEventListener("submit"
     });
 
-})
 
+
+    
+
+
+
+
+// function saveLocalTodos(todo) {
+//     let todos;
+//     if(localStorage.getItem("todos") === null) {
+//         todos = [];
+//     } else {
+//         todos = JSON.parse(localStorage.getItem("todos"));
+//     }
+//     todos.push(todo);
+//     localStorage.setItem("todos", JSON.stringify(todos));
+//     }
+
+//     document.addEventListener("DOMContentLoaded", getLocalTodos);
+
+//     function getLocalTodos() {
+//         let todos;
+//         if (localStorage.getItem("todos") === null) {
+//             todos = [];
+//         } else {
+//             todos = JSON.parse(localStorage.getItem("todos"));
+//         }
+//     }
+ 
+
+
+
+
+
+//end window.addEventListener('load'
+});
 
 
